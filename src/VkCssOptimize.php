4982177844006d3f271a36362fea3c4c70ce1dba
drill-lancer <rickaddison7634@gmail.com>
2024-09-27 10:31:40 +0900

    fix: filter hook name

--- src/VkCssOptimize.php
+++ src/VkCssOptimize.php
@@ -488,7 +488,7 @@ class VkCssOptimize {
 					'btn',
 					'btn-primary',
 				);
-				$jsaddlist = apply_filters( 'css_tree_shaking_js_added_classes', $jsaddlist );
+				$jsaddlist = apply_filters( 'css_tree_shaking_js_added_class', $jsaddlist );
 
 				// CSS の中身が取得できた場合のみ Tree Shaking を反映する
 				if ( ! empty( $css ) ) {
